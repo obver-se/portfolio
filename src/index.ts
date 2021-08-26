c0edd8c48b022c3a6e19994ea66aa84f3ff66b0c
obver-se <o@obver.se>
2021-08-25 17:27:23 -0700

    Added a shadow box so that project images can be zoomed to full window and then scrolled through *doesn't support videos

--- src/index.ts
+++ src/index.ts
@@ -40,6 +40,72 @@ function registerNamedAnchors() {
   });
 }
 
+/*
+ * Make all images in ".expandable-images" elements part of seperate image display groups
+ * that can be clicked, expanded, and then cycled through
+ */
+function makeImagesExpandable() {
+  // The shared shadowbox for images
+  const shadowBox = document.getElementById('image-shadowbox');
+  const closeBoxFunction = () => {
+    shadowBox.style.display = 'none';
+    document.body.style.overflow = null;
+  };
+  document.getElementById('close').addEventListener('click', closeBoxFunction);
+  shadowBox.addEventListener('click', closeBoxFunction);
+  // Each grouping of images
+  const imageGroups = document.getElementsByClassName('expandable-images');
+
+  Array.from(imageGroups).forEach((grouping) => {
+    const imagesInGroup = grouping.getElementsByTagName('img');
+    for (let i = 0; i < imagesInGroup.length; i += 1) {
+      // Create variables that represent what image to go to when the
+      // user hits the next or last button while handling looping around
+      let lastImage: HTMLElement;
+      let nextImage: HTMLElement;
+      if (i === 0) {
+        lastImage = imagesInGroup[imagesInGroup.length - 1];
+      } else {
+        lastImage = imagesInGroup[i - 1];
+      }
+
+      if (i === (imagesInGroup.length - 1)) {
+        nextImage = imagesInGroup[0];
+      } else {
+        nextImage = imagesInGroup[i + 1];
+      }
+      // A variable that represents the current image itself
+      const image = imagesInGroup[i];
+      // Add a pointer cursor when the user hovers over the image to display
+      image.style.cursor = 'pointer';
+
+      // When the user clicks on the image to display...
+      image.addEventListener('click', () => {
+        // Display the shadowBox
+        const url = `url(${image.src})`;
+        document.getElementById('center-image').style.backgroundImage = url;
+        shadowBox.style.display = 'flex';
+        // Disable scrolling
+        document.body.style.overflow = 'hidden';
+
+        // Handle the previous button
+        document.getElementById('prev').onclick = (e) => {
+          e.stopPropagation();
+          // "Click" on the image that would be next
+          lastImage.click();
+        };
+        // handle the next button
+        document.getElementById('next').onclick = (e) => {
+          e.stopPropagation();
+          // "Click" on the image that would be next
+          nextImage.click();
+        };
+      });
+    }
+  });
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   registerNamedAnchors();
+  makeImagesExpandable();
 });
